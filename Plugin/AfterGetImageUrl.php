847a8059a7f9aeae39a3df90f60d31b8068c30a6
gideon <gideon@elgentos.nl>
2020-09-15 11:50:39 +0200

    Resolve issue with loading small images

--- Plugin/AfterGetImageUrl.php
+++ Plugin/AfterGetImageUrl.php
@@ -2,6 +2,8 @@
 
 namespace Elgentos\Imgix\Plugin;
 
+use Magento\Framework\Exception\NoSuchEntityException;
+
 class AfterGetImageUrl
 {
     /**
@@ -9,10 +11,24 @@ class AfterGetImageUrl
      */
     protected $image;
 
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    /**
+     * @var \Magento\Catalog\Helper\ImageFactory
+     */
+    protected $imageHelperFactory;
+
     public function __construct(
-        \Elgentos\Imgix\Model\Image $image
+        \Elgentos\Imgix\Model\Image $image,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Catalog\Helper\ImageFactory $imageHelperFactory
     ) {
         $this->image = $image;
+        $this->productRepository = $productRepository;
+        $this->imageHelperFactory = $imageHelperFactory;
     }
 
     public function after__call(\Magento\Catalog\Block\Product\Image $image, $result, $method)
@@ -24,7 +40,11 @@ class AfterGetImageUrl
         try {
             if ($method == 'getImageUrl' && $image->getProductId() > 0) {
                 if ($image->getHeight() < 300 || $image->getWidth() < 300) {
-                    return $this->image->getSmallUrl($result);
+                    $defaultImage = $this->getDefaultImageUrl($image->getProductId());
+                    if(! $defaultImage) {
+                        return $result;
+                    }
+                    return $this->image->getSmallUrl($defaultImage);
                 }
                 return $this->image->getDefaultUrl($result);
             }
@@ -32,5 +52,29 @@ class AfterGetImageUrl
         }
 
         return $result;
+    }
+
+    public function getDefaultImageUrl(int $productId): ?string {
+        $product = $this->getProductById($productId);
+        if(! $product) {
+            return null;
+        }
+
+        return $this->imageHelperFactory->create()
+            ->init($product, 'product_page_main_image')->getUrl();
+    }
+
+    /**
+     * @param $id
+     * @return \Magento\Catalog\Api\Data\ProductInterface
+     * @throws NoSuchEntityException
+     */
+    public function getProductById($id)
+    {
+        try {
+            return $this->productRepository->getById($id);
+        } catch (NoSuchEntityException $noSuchEntityException) {
+            return null;
+        }
     }
 }
--- Plugin/AfterGetImageUrlHelper.php
+++ Plugin/AfterGetImageUrlHelper.php
@@ -9,20 +9,35 @@ class AfterGetImageUrlHelper
      */
     protected $image;
 
+    /**
+     * @var \Magento\Catalog\Helper\ImageFactory
+     */
+    protected $imageHelperFactory;
+
     public function __construct(
-        \Elgentos\Imgix\Model\Image $image
+        \Elgentos\Imgix\Model\Image $image,
+        \Magento\Catalog\Helper\ImageFactory $imageHelperFactory
     ) {
         $this->image = $image;
+        $this->imageHelperFactory = $imageHelperFactory;
     }
 
-    public function afterGetImageUrl($subject, $result, $product, $imageTypeId = 'product_base_image', $w = null, $h = null)
-    {
-        if(! $this->image->isServiceEnabled()) {
+    public function afterGetImageUrl(
+        $subject,
+        $result,
+        $product,
+        $imageTypeId = 'product_base_image',
+        $w = null,
+        $h = null
+    ) {
+        if (! $this->image->isServiceEnabled()) {
             return $result;
         }
 
         if ($h < 300 || $w < 300) {
-            return $this->image->getSmallUrl($result);
+            $defaultImage = $this->imageHelperFactory->create()
+                ->init($product, 'product_page_main_image')->getUrl();
+            return $this->image->getSmallUrl($defaultImage);
         }
 
         return $this->image->getDefaultUrl($result);
